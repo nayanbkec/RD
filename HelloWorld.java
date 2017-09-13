65133316e54223bfe87cecc15c93678e3a5b48b0
unknown <nayanbkec@yahoo.com>
2017-09-13 08:52:01 +0530

    test

--- HelloWorld.java
+++ HelloWorld.java
@@ -6,4 +6,9 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello world");
 	}
+	
+	public static void method1(String[] args) {
+		System.out.println("Hello world");
+	}
+
 }
